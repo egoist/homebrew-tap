443337d6fe7bf723c700c4ad2fd8fdfc75ace36e
EGOIST <0x142857@gmail.com>
2021-12-07 22:09:26 +0800

    update dum

--- Formula/dum.rb
+++ Formula/dum.rb
@@ -2,14 +2,14 @@
 class Dum < Formula
   desc "An npm scripts runner written in Rust."
   homepage "https://github.com/egoist/dum"
-  version "0.1.14"
+  version "0.1.15"
 
   
 on_macos do
   
   if Hardware::CPU.arm?
-    url "https://github.com/egoist/dum/releases/download/v0.1.14/dum-aarch64-apple-darwin.zip"
-    sha256 "a1dfe2567fdc723827b9004edf0aa4b18add488035ce23df707d39692ab6b0ad"
+    url "https://github.com/egoist/dum/releases/download/v0.1.15/dum-aarch64-apple-darwin.zip"
+    sha256 "b4f21fcbf0995b642a6e021db6c0c3e458e7d19bbbd1b07f82344b9bb3dbf58f"
 
     def install
       bin.install "dum"
@@ -18,8 +18,8 @@ on_macos do
   
 
   if Hardware::CPU.intel?
-    url "https://github.com/egoist/dum/releases/download/v0.1.14/dum-x86_64-apple-darwin.zip"
-    sha256 "591564821b1ce6f8f60274505057582498ffe9f0ab5f4d957e00931ce9092803"
+    url "https://github.com/egoist/dum/releases/download/v0.1.15/dum-x86_64-apple-darwin.zip"
+    sha256 "d0d5d27fb558f8de1556cef7a802c01a4a52305ecec4637636bbf30d713b266d"
 
     def install
       bin.install "dum"
@@ -33,8 +33,8 @@ end
 on_linux do
   
   if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/egoist/dum/releases/download/v0.1.14/dum-arm-unknown-linux-musleabihf.tar.gz"
-    sha256 "2b8f8624ebea9d59804c469fabe17f538681b8448b1fe7728619de0c3bd6ff9d"
+    url "https://github.com/egoist/dum/releases/download/v0.1.15/dum-aarch64-unknown-linux-musl.tar.gz"
+    sha256 "f4d37996a9e20e67fb70aaa7582d99ce8fa40d7f5842ce988288b89f9191f48e"
 
     def install
       bin.install "dum"
@@ -43,8 +43,8 @@ on_linux do
   
 
   if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-    url "https://github.com/egoist/dum/releases/download/v0.1.14/dum-x86_64-unknown-linux-musl.tar.gz"
-    sha256 "2ae19dfe36d3b3fe359790f1e8d90ce309a3a055be6d87c49f973fb140451e1d"
+    url "https://github.com/egoist/dum/releases/download/v0.1.15/dum-x86_64-unknown-linux-musl.tar.gz"
+    sha256 "66bce6a5209d4afc0baffa879bbd1d120cc5f2341d0e6bb6d63b24ba0cab59cd"
 
     def install
       bin.install "dum"

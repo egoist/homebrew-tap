37d7706b0a43d74e8297c8c54021fb8f85d7991a
EGOIST <0x142857@gmail.com>
2021-12-08 09:12:14 +0800

    update dum

--- Formula/dum.rb
+++ Formula/dum.rb
@@ -7,9 +7,9 @@ class Dum < Formula
   
 on_macos do
   
-  if Hardware::CPU.intel?
-    url "https://github.com/egoist/dum/releases/download/v0.1.17/dum-x86_64-apple-darwin.zip"
-    sha256 "d5bfdacc793444a35c3840b23e5716d97502360288ffee7b155a77cec019c740"
+  if Hardware::CPU.arm?
+    url "https://github.com/egoist/dum/releases/download/v0.1.17/dum-aarch64-apple-darwin.zip"
+    sha256 "4fd87caf5ee03b2298ad9b5bc072e019c4973af2ebb02377a5a7a84a22af337f"
 
     def install
       bin.install "dum"
@@ -17,9 +17,9 @@ on_macos do
   end
   
 
-  if Hardware::CPU.arm?
-    url "https://github.com/egoist/dum/releases/download/v0.1.17/dum-aarch64-apple-darwin.zip"
-    sha256 "2a64de907ec32db33bc05878f012e1e1cdb2af31fda3e5f0de9e46094016c4dd"
+  if Hardware::CPU.intel?
+    url "https://github.com/egoist/dum/releases/download/v0.1.17/dum-x86_64-apple-darwin.zip"
+    sha256 "2311b6881a4e69f446f689888d4c255812429643ebe136f526a2607993f619bb"
 
     def install
       bin.install "dum"
@@ -34,7 +34,17 @@ on_linux do
   
   if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
     url "https://github.com/egoist/dum/releases/download/v0.1.17/dum-aarch64-unknown-linux-musl.tar.gz"
-    sha256 "eab499c71909516f16240d847cc07a2f20e452802718b3d524594844f2e9fd99"
+    sha256 "c25aab9b259b93305dc992c6d89ee4af3aeedc222ffa5054148e3b4c7bdb37cb"
+
+    def install
+      bin.install "dum"
+    end
+  end
+  
+
+  if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
+    url "https://github.com/egoist/dum/releases/download/v0.1.17/dum-x86_64-unknown-linux-musl.tar.gz"
+    sha256 "3a6836e2241622384d25f4160bc6c619c2eb16bab0641604929372545a8a85fe"
 
     def install
       bin.install "dum"

bce54880097faa546cd811df7bb188cc09edf1fb
goreleaserbot <goreleaser@carlosbecker.com>
2022-01-28 19:00:48 +0800

    Brew formula update for bget version v1.0.8

--- Formula/bget.rb
+++ Formula/bget.rb
@@ -5,20 +5,20 @@
 class Bget < Formula
   desc ""
   homepage ""
-  version "1.0.7"
+  version "1.0.8"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/egoist/bget/releases/download/v1.0.7/bget_1.0.7_Darwin_x86_64.tar.gz"
-      sha256 "5b52b839e67361bb335de7f40bf6a0de1a98014b09aa6c48a46f5d08fc085583"
+      url "https://github.com/egoist/bget/releases/download/v1.0.8/bget_1.0.8_Darwin_x86_64.tar.gz"
+      sha256 "720f15cb03644c7e6b1d6bb97517f3d82ded91ce98b82685d31a9d86a54e2dfa"
 
       def install
         bin.install "bget"
       end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/egoist/bget/releases/download/v1.0.7/bget_1.0.7_Darwin_arm64.tar.gz"
-      sha256 "59949e122acb5b75aa4b7cab521f5f8519ff71f0c7c66e5b97381570c24dfffe"
+      url "https://github.com/egoist/bget/releases/download/v1.0.8/bget_1.0.8_Darwin_arm64.tar.gz"
+      sha256 "86eba98ea1fa2bb256f4c716b00222f9b32c9a23ed26dd92613f2aea37236931"
 
       def install
         bin.install "bget"
@@ -28,16 +28,16 @@ class Bget < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/egoist/bget/releases/download/v1.0.7/bget_1.0.7_Linux_arm64.tar.gz"
-      sha256 "81b2f036751204efea6bc6c40b95975a80e956ac3e5e410e4fbe394097523cbc"
+      url "https://github.com/egoist/bget/releases/download/v1.0.8/bget_1.0.8_Linux_arm64.tar.gz"
+      sha256 "094194a3ca4f83c8c5bb217add8b2f9eb8223e664300feaf581ab8f521a7db7a"
 
       def install
         bin.install "bget"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/egoist/bget/releases/download/v1.0.7/bget_1.0.7_Linux_x86_64.tar.gz"
-      sha256 "402277dbf41fdad15585a20566dfa2707f48e31310e6cf6a53d022ec835cf387"
+      url "https://github.com/egoist/bget/releases/download/v1.0.8/bget_1.0.8_Linux_x86_64.tar.gz"
+      sha256 "437abce1fd8060b292efb2565e69e3c38753d80ffec3bda1ea06fb67db3530f7"
 
       def install
         bin.install "bget"
